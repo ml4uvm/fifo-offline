aebb51dd062ccf2cf6f6dcca842255d212ad2aaf
anusreechakravarthy <anusree202210@gmail.com>
2026-04-27 19:36:21 +0530

    test_fifo updated with clock. alu_test was deleted earlier

--- tb/tests/fifo_test.py
+++ tb/tests/fifo_test.py
@@ -2,7 +2,8 @@ import cocotb
 from pyuvm import uvm_test, uvm_root
 from tb.components.env import FIFOEnv
 from tb.sequences.sequence import FIFOSequence
-
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge
 
 class FIFOTest(uvm_test):
 
@@ -15,12 +16,12 @@ class FIFOTest(uvm_test):
         # =====================================================
         # BASELINE MODE (random)
         # =====================================================
-        # seq = FIFOSequence("seq", num_tests=300, use_ml=False)
+        seq = FIFOSequence("seq", num_tests=300, use_ml=False)
 
         # =====================================================
         # ML MODE (clustered testcases)
         # =====================================================
-        seq = FIFOSequence("seq", use_ml=True)
+        #seq = FIFOSequence("seq", use_ml=True)
 
         await seq.start(self.env.agent.seqr)
 
@@ -29,4 +30,14 @@ class FIFOTest(uvm_test):
 
 @cocotb.test()
 async def run_test(dut):
+
+    # 🔥 ADD CLOCK
+    clock = Clock(dut.clk, 10, units="ns")
+    cocotb.start_soon(clock.start())
+
+    # Optional reset stabilization
+    dut.rst.value = 1
+    await RisingEdge(dut.clk)
+    dut.rst.value = 0
+
     await uvm_root().run_test("FIFOTest")
